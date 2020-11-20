766306112995aa4721a8385cf209c03b10d20606
NiutaR <69516148+NiutaR@users.noreply.github.com>
2020-11-20 20:49:12 +0200

    added In-app Navigation

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -8,14 +11,25 @@ import { Hero } from '../hero';
 })
 
 export class HeroDetailComponent implements OnInit {
-  @Input() hero: Hero | null;
-  
+  hero: Hero;
 
-  constructor() { 
-    this.hero = null;
-  }
+
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) { }
 
   ngOnInit(): void {
+    this.getHero();
   }
-
+  getHero(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.heroService.getHero(Number(id))
+      .subscribe(hero => this.hero = hero);
+  }
+  goBack(): void {
+    this.location.back();
+  }
 }
+
